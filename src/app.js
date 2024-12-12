0760b33505f11df819a55ad0ed5d2c071f6b5863
Mohit sharma <mohitanand8987@gmail.com>
2024-12-12 23:10:45 +0530

    update API with id

--- src/app.js
+++ src/app.js
@@ -32,6 +32,37 @@ app.get("/userId", async (req, res) => {
     res.status(400).send("Something went wrong");
   }
 });
+// DELETE user by ID
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User deleted successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+// Update data from User
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const body = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, body, {
+      returnDocument: "after",
+    });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("user updated successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 // feed API - GET/ feed - get all users from the database
 app.get("/feed", async (req, res) => {
   try {
