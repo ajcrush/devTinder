6bef728cbbbdc65458ae9812779c1d61bd0329d9
Mohit sharma <mohitanand8987@gmail.com>
2024-12-12 21:14:32 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,43 @@ const connectDB = require("./config/database");
 const User = require("./config/models/user");
 
 app.use(express.json());
+// Get user by Email(/user)
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+// Get user by ID(/userId)
+app.get("/userId", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+// feed API - GET/ feed - get all users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
