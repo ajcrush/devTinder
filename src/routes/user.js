0d85374270f8208d6858dfdc19c3fddba6ae0779
Mohit sharma <mohitanand8987@gmail.com>
2024-12-22 21:15:38 +0530

    solved some bug

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,23 +2,48 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../config/models/connectionRequest");
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "gender",
+  "age",
+  "skills",
+  "photoUrl",
+];
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "gender",
-      "age",
-      "skills",
-      "photoUrl",
-    ]);
+    }).populate("fromUserId", USER_SAFE_DATA);
     res.json({
       message: "Data fetched successfully",
       data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({
+      data,
     });
   } catch (err) {
     res.status(400).send("Error : " + err.message);
